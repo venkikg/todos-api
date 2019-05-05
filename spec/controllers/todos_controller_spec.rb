fceb6c304bb7d8df34907c0a39394dde480600a5
VG030642 <venkatesh.g@cerner.com>
2019-05-05 20:45:34 +0530

    first commit

---
+++ spec/controllers/todos_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe TodosController, type: :controller do
+
+  describe "GET #items" do
+    it "returns http success" do
+      #get :items
+      #expect(response).to have_http_status(:success)
+    end
+  end
+
+end
